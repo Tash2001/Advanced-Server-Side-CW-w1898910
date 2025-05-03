3b6ba2000bb4a7700cd6649053b2e4aa9c255fef
tashini maleesha <tashini.maleesha@gmail.com>
2025-05-04 03:24:50 +0530

    microservices - auth

---
+++ CW2/auth-service/routers/authRoutes.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const { register, login } = require('../controller/authController');
+const router = express.Router();
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: User authentication routes
+ */
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/register',register);
+
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Log in a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful with JWT token
+ *       401:
+ *         description: Invalid credentials
+ */
+router.post('/login',login);
+
+module.exports= router;
